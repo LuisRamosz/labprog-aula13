f4e9dbf14242e781adf759efd51db996a8c48578
LuisRamosz <luisfelipesr48@gmail.com>
2024-10-08 21:36:45 -0300

    tde13

---
+++ Estagiario.cs
@@ -0,0 +1,13 @@
+public class Estagiario : Funcionario
+{
+    public override double CalcularSalario()
+    {
+        return base.CalcularSalario() / 2;
+    }
+
+    public Estagiario(string nome, int idade)
+    :base(nome, idade)
+    {
+
+    }
+}
